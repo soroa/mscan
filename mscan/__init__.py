593b3d6e8208f1a63c405f431c7cf98e4ff00d17
soroa <soroa@student.ethz.ch>
2016-05-28 15:20:14 +0200

    error messages

--- mscan/__init__.py
+++ mscan/__init__.py
@@ -62,38 +62,38 @@ def users_upload():
 @app.route('/viewMD/<userID>')
 def viewMD(userID):
 	user=User.query.filter_by(user_id=userID).first()
-		if user:
-			mds = db.session.query(MobileData).order_by(desc(MobileData.md_creation_time)).all()
-			return render_template('mds.html', mds = mds)
-		else:
-			return """<html><body>
-			This user is not in the database
-			</body></html>""" 
+	if user:
+		mds = db.session.query(MobileData).order_by(desc(MobileData.md_creation_time)).all()
+		return render_template('mds.html', mds = mds)
+	else:
+		return """<html><body>
+		This user is not in the database
+		</body></html>""" 
 
 
 
 @app.route('/viewCalls/<userID>')
 def viewCalls(userID):
 	user=User.query.filter_by(user_id=userID).first()
-		if user:
-			calls = db.session.query(Call).order_by(desc(Call.call_creation_time)).all()
-			return render_template('calls.html', calls = calls)
-		else:
-			return """<html><body>
-			This user is not in the database
-			</body></html>"""
+	if user:
+		calls = db.session.query(Call).order_by(desc(Call.call_creation_time)).all()
+		return render_template('calls.html', calls = calls)
+	else:
+		return """<html><body>
+		This user is not in the database
+		</body></html>"""
 
 
 @app.route('/viewSMS/<userID>')
 def viewSMS(userID):
 	user=User.query.filter_by(user_id=userID).first()
-		if user:
-			sms = db.session.query(SMS).order_by(desc(SMS.sms_creation_time)).all()
-			return render_template('sms.html', sms = sms)
-		else:
-			return """<html><body>
-			This user is not in the database
-			</body></html>"""
+	if user:
+		sms = db.session.query(SMS).order_by(desc(SMS.sms_creation_time)).all()
+		return render_template('sms.html', sms = sms)
+	else:
+		return """<html><body>
+		This user is not in the database
+		</body></html>"""
 
 
 @app.route('/viewUsers')
