121fd0d5192d9b49e748097c0ed4c2df727b92d0
soroa <soroa@student.ethz.ch>
2016-06-15 11:18:25 +0200

    Added function to test whether filter fucntions work with database

--- mscan/dbFunctions.py
+++ mscan/dbFunctions.py
@@ -235,7 +235,7 @@ def locationIsCH(location):
 def getLastXDaysSMS(user_ID,x):
 	daysback = datetime.timedelta(days=x)
 	since = datetime.datetime.now() - daysback
-	print("type of return value is "+ str(type(SMS.query.filter(and_(SMS.sms_creation_time > since, SMS.user_id==user_ID) ).all)))
+	print("type of return value is "+ str(type(SMS.query.filter(and_(SMS.sms_creation_time > since, SMS.user_id==user_ID) ).all())))
 	return SMS.query.filter(and_(SMS.sms_creation_time > since, SMS.user_id==user_ID) ).all
 
 def getLastXDaysCalls(user_ID, x):
