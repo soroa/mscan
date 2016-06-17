5b3ebd0be22fb0629e4416259721aeaa2b1dab58
soroa <soroa@student.ethz.ch>
2016-06-17 11:44:43 +0200

    ISO tranlsation now happens on the server

--- mscan/__init__.py
+++ mscan/__init__.py
@@ -338,7 +338,9 @@ def uploadCountryISOLog(user_ID):
 				cISO.cISO_creation_time = datetime.datetime.strptime(data_entry.get('iso_entry_creation_time'), '%Y-%m-%d %H:%M:%S')
 				iso = data_entry.get('countryISO')
 				countryNameEN =pycountry.countries.get(alpha2=iso).name
-				print("iso is " + iso + " and country from pycountry is " + countryNameEN + " translated to "+ resources.translation_table.get(countryNameEN))
+				isDuplicate = CountryISOLog.query.filter(and_(CountryISOLog.cISO_creation_time==cISO.cISO_creation_time ,CountryISOLog.cISO_user_id == cISO.cISO_user_id)).first()
+				if isDuplicate:
+					continue
 				cISO.cISO_countryISO =resources.translation_table.get(countryNameEN)
 				db.session.add(cISO)
 				db.session.commit()
