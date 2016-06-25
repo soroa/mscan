c6f5ee555f17245983fc7d847c1d8f3c01dc7435
ma <mac@public-docking-hg-2506.ethz.ch>
2016-06-25 11:52:49 +0200

    printing the request

--- mscan/dbFunctions.py
+++ mscan/dbFunctions.py
@@ -12,7 +12,7 @@ def getUserAgeField(user_ID):
 	user=User.query.filter_by(user_id=user_ID).first()
 	if user:
 		age = int(user.age)
-		print("age is  " + age)
+		print("age is  " +str(age))
 		if age<18:
 			return 15
 		elif age<26:
