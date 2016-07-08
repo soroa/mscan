b9c2c661324d5c07d56ecd533238eed34de048ef
ma <mac@public-docking-cx-0476.ethz.ch>
2016-07-08 12:43:22 +0200

    request changed again

--- mscan/__init__.py
+++ mscan/__init__.py
@@ -309,6 +309,8 @@ def getContracts(user_ID):
 		user=User.query.filter_by(user_id=user_ID).first()
 		if user:
 			reg_date = user.registration_datetime
+			if getDaysSinceSignUp(user_ID)<3:
+				return jsonify(message="registration too recent")
 			#todo	don't send contracts if the user just registered
 			contractsDictArray = getContractsDict(user_ID)
 
