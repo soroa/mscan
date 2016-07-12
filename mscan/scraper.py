eb935ef4765cb5414786853d4b0465573a854a32
ma <mac@public-docking-hg-0640.ethz.ch>
2016-07-12 19:18:42 +0200

    request changed again

--- mscan/scraper.py
+++ mscan/scraper.py
@@ -135,9 +135,9 @@ def getContractsDict(user_ID):
 	# print(formDataExt)
 
 	headers = { 
-	'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+	'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
 	'Accept-Encoding':'gzip, deflate',
-	'Accept-Language':'en-US,en;q=0.8',
+	'Accept-Language':'en-US,en;q=0.5',
 	'Connection':'keep-alive',
 	'Content-Type':'application/x-www-form-urlencoded',
 	'Cookie':sessionID,
